2342d7a7f7855e4d528b92c335bfa9d7c6c2a5d2
Max Zampieri <98056019+MaxZampieri@users.noreply.github.com>
2022-07-03 17:56:43 -0300

    animation cards project

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
-document.addEventListener('DOMContentLoaded', function(){
-    document.querySelector('#skills')
-            .classList
-            .add('loaded');
+const appear = document.querySelector('.appear'); 
+const cb = function(entries){
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.classList.add('inview');
+    }else{
+      entry.target.classList.remove('inview');
+    }
   });
- 
+}
+const io = new IntersectionObserver(cb);
+io.observe(appear);
 
  
+
+
+
+
+const items = document.querySelectorAll('.appear2');
+
+const active = function(entries){
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+        entry.target.classList.add('inview2'); 
+        }else{
+            entry.target.classList.remove('inview2'); 
+        }
+    });
+}
+const io2 = new IntersectionObserver(active);
+for(let i=0; i < items.length; i++){
+    io2.observe(items[i]);
+ }
